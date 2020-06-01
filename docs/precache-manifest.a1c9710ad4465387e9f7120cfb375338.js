434fac2b0e1ce2ccc149b641ccbd2955629dbdca
Abe Fehr <abe.fehr@gmail.com>
2020-05-31 22:41:10 -0400

    Another attempt

---
+++ docs/precache-manifest.a1c9710ad4465387e9f7120cfb375338.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd5e719b0f430cccf3324262352ee272",
+    "url": "/weird-flex-but-ok/index.html"
+  },
+  {
+    "revision": "93adeb0e08dc1b391a0f",
+    "url": "/weird-flex-but-ok/static/css/2.38447d4d.chunk.css"
+  },
+  {
+    "revision": "6c2571bc76b02ae3a9a1",
+    "url": "/weird-flex-but-ok/static/css/main.295e2af1.chunk.css"
+  },
+  {
+    "revision": "93adeb0e08dc1b391a0f",
+    "url": "/weird-flex-but-ok/static/js/2.9ce8fdfd.chunk.js"
+  },
+  {
+    "revision": "e1ebcee444532ceab7d2a4a1c7007bba",
+    "url": "/weird-flex-but-ok/static/js/2.9ce8fdfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c2571bc76b02ae3a9a1",
+    "url": "/weird-flex-but-ok/static/js/main.6b35ccd3.chunk.js"
+  },
+  {
+    "revision": "0717a8ad9b54c6244037",
+    "url": "/weird-flex-but-ok/static/js/runtime-main.234621d8.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.155681c178fdb4a747e1e695b0905a11.js"
+  "/weird-flex-but-ok/precache-manifest.a1c9710ad4465387e9f7120cfb375338.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weird-flex-but-ok/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
